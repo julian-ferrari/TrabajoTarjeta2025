3952c54900e87e3aa022648cd1ba8a725f9c74fb
Julián Ferrari <julianferrarijuli@gmail.com>
2025-11-04 20:24:05 -0300

    Añadidos Archivos Para Mejorar Manejo de Tiempo y Cobertura

---
+++ TrabajoTarjeta/ITiempoProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabajoTarjeta
+{
+    /// <summary>
+    /// Interfaz para abstracción del tiempo. Permite usar tiempo real o falso en tests.
+    /// </summary>
+    public interface ITiempoProvider
+    {
+        DateTime Now();
+    }
+}
